37e557c90f05109a0aeebc322920006cb34969bc
Zhenning Ruan <rzning@outlook.com>
2019-11-20 22:18:08 +0800

    Update 2017208893 : fiction07
    废土 : 西方名家末世故事集 : 7 等待微风号

--- books/.vuepress/config.js
+++ books/.vuepress/config.js
@@ -56,7 +56,8 @@ module.exports = {
             ['fiction03', '面包与炸弹'],
             ['fiction04', '进镇出镇记'],
             ['fiction05', '沙渣之族'],
-            ['fiction06', '隧道暗且长']
+            ['fiction06', '隧道暗且长'],
+            ['fiction07', '等待微风号']
           ]
         }
       ],
