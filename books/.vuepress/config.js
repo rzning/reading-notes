0e31e7916a51a1130c31e746d2d42990112353e5
Zhenning Ruan <rzning@outlook.com>
2019-12-05 22:59:27 +0800

    Update configuration/sidebar

--- books/.vuepress/config.js
+++ books/.vuepress/config.js
@@ -1,3 +1,5 @@
+const sidebar = require('./configuration/sidebar')
+
 module.exports = {
   title: '读书笔记 - rzning.com',
   description: '阮振宁的日常读书笔记',
@@ -16,64 +18,6 @@ module.exports = {
     ],
     sidebarDepth: 2,
     // displayAllHeaders: true,
-    sidebar: {
-      '/F713/2018095142/': [
-        '',
-        {
-          title: 'Contents',
-          collapsable: false,
-          children: [
-            ['chapter01', '第一章 区块链概论'],
-            ['chapter02', '第二章 以太坊工作原理与基础'],
-            ['chapter03', '第三章 以太坊安装与开发环境配置'],
-            ['chapter04', '第四章 以太坊应用接口']
-          ]
-        }
-      ],
-      '/F713/2018184395/': [
-        '',
-        {
-          title: 'Contents',
-          collapsable: false,
-          children: [
-            ['chapter01', '第一章 引言'],
-            ['chapter02', '第二章 比特币'],
-            ['chapter03', '第三章 以太坊'],
-            ['chapter04', '第四章 企业级区块链'],
-            ['chapter05', '第五章 Quorum']
-          ]
-        },
-      ],
-      '/I712/2017208893/': [
-        '',
-        {
-          title: 'Wastelands',
-          collapsable: false,
-          children: [
-            ['preface', '序'],
-            ['fiction01', '一切混乱的终结'],
-            ['fiction02', '回收'],
-            ['fiction03', '面包与炸弹'],
-            ['fiction04', '进镇出镇记'],
-            ['fiction05', '沙渣之族'],
-            ['fiction06', '隧道暗且长'],
-            ['fiction07', '等待微风号'],
-            ['fiction08', '永不绝望'],
-            ['fiction09', '系统管理员照看世界'],
-            ['fiction10', '最后的原生生物'],
-            ['fiction11', '末日一瞥']
-          ]
-        }
-      ],
-      'TP312/2015033934': [
-        '',
-        {
-          title: 'Contents',
-          collapsable: false,
-          children: []
-        }
-      ],
-      '/': false
-    }
+    sidebar
   }
 }
---
+++ books/.vuepress/configuration/sidebar/TP312_2015033934.js
@@ -0,0 +1,14 @@
+module.exports = [
+  '',
+  {
+    title: 'PART 1 Scope & Closures',
+    collapsable: false,
+    children: [
+      ['part_one/chapter01', '1 作用域是什么']
+    ]
+  },
+  {
+    title: 'PART 2 this & Object Prototypes',
+    collapsable: false
+  }
+]
