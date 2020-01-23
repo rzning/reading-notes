75c0b9b536641539ee50b745c0e28be77eae15fd
Zhenning Ruan <rzning@outlook.com>
2020-01-23 22:44:14 +0800

    Create TP312/2013063408/chapter04.md
    JavaScript 设计模式 - 4 - 设计模式的结构

--- books/.vuepress/configuration/sidebar/TP312_2013063408.js
+++ books/.vuepress/configuration/sidebar/TP312_2013063408.js
@@ -6,7 +6,8 @@ module.exports = [
     children: [
       ['chapter01', '第一章 介绍'],
       ['chapter02', '第二章 什么是模式'],
-      ['chapter03', '第三章 模式状态测试']
+      ['chapter03', '第三章 模式状态测试'],
+      ['chapter04', '第四章 设计模式的结构']
     ]
   }
 ]
