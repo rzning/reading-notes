07afcf6db19f4ae1544790ea0dff2da036d29fab
Zhenning Ruan <rzning@outlook.com>
2020-01-23 22:15:07 +0800

    Create TP312/2013063408/chapter03.md
    JavaScript 设计模式 - 3 - 模式状态测试

--- books/.vuepress/configuration/sidebar/TP312_2013063408.js
+++ books/.vuepress/configuration/sidebar/TP312_2013063408.js
@@ -5,7 +5,8 @@ module.exports = [
     collapsable: false,
     children: [
       ['chapter01', '第一章 介绍'],
-      ['chapter02', '第二章 什么是模式']
+      ['chapter02', '第二章 什么是模式'],
+      ['chapter03', '第三章 模式状态测试']
     ]
   }
 ]
