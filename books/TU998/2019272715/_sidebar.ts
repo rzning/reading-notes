51222d76417387485b4ed7c5ca034651e43e7917
Zhenning Ruan <rzning@outlook.com>
2023-06-16 23:10:13 +0800

    Create [ TU998/2019272715 ] chapter_2_1.md

--- books/TU998/2019272715/_sidebar.ts
+++ books/TU998/2019272715/_sidebar.ts
@@ -8,7 +8,8 @@ const sidebar: SidebarConfigArray = [
     children: ['chapter_1_1', 'chapter_1_2']
   },
   {
-    text: '二、消防工作概述'
+    text: '二、消防工作概述',
+    children: ['chapter_2_1']
   },
   {
     text: '三、燃烧和火灾'
